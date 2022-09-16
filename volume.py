8ecc73b0e374f2632a6a7e0d69485b91bf32dcdb
Mateus Schiavi <mtsschiavi@gmail.com>
2022-09-16 13:53:49 -0300

    a

--- volume.py
+++ volume.py
@@ -5,27 +5,27 @@
 import math
 
 print("Welcome to Tire Volume Calculator")
-print("*********************************")
+print(":::::::::::::::::::::::::::::::::")
 
 def main():
     width = int(input("Enter the width of the tire in mm: "))
-    aspect_ratio = int(input("Enter the aspect ratio of the tire: "))
-    diameter = int(input("Enter the diameter of the wheel in inches: "))
+    respective_ratio = int(input("Enter the aspect ratio of the tire: "))
+    respective_diameter = int(input("Enter the diameter of the wheel in inches: "))
 
-    volume = round(((math.pi * width ** 2 * aspect_ratio * (width * aspect_ratio + 2540 * diameter)) / 10000000), 1)
+    volume = round(((math.pi * width ** 2 * respective_ratio * (width * aspect_ratio + 2540 * respective_diameter)) / 10000000), 1)
 
-    print(f"The approximate volume is {volume} milliliters.")
+    print(f"The respective volume is {volume} milliliters.")
     repeat()
 
 def repeat():
-    answer = input("Would you like to calculate another volume (Y/N)? ")
+    answer = input("Would you like to compute another volume (Y/N)? ")
     if answer.upper() == "Y":
         main()
     if answer.upper() == "N":
-        print("The program will now close")
+        print("The program will now end")
         exit()
     else:
-        print("Error: Invalid input")
+        print("Error: Invalid Argument")
         repeat()
 
 main()
