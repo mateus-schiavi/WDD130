1cb6cdec75b8090ad94c99218c9d52da6785495d
Mateus Schiavi <mtsschiavi@gmail.com>
2022-09-23 15:55:25 -0300

    aa

--- date_and_time.py
+++ date_and_time.py
@@ -1,29 +1,45 @@
+#Mateus Schiavi
+#CSE 111
+#01 Prove Milestone
+
 import math
+
 from datetime import datetime
-loop = True
 
-def tire_volume_function(width_tire, aspect_tire, diameter_tire):
-    return (math.pi * width_tire * width_tire * aspect_tire * (width_tire * aspect_tire + 2540 * diameter_tire)) / 10000000
+running = False
 
-print("Enter the width of the tire in mm (ex 205):")
-width_tire = int(input(">> "))
-print("Enter the aspect ratio of the tire (ex 60):")
-aspect_tire = int(input(">> "))
-print("Enter the diameter of the wheel in inches (ex 15):")
-diameter_tire = int(input(">> "))
+print("Welcome to Tire Volume Calculator")
+print("|:|:|:|:|:|:|:|:|:|:|:|:|:|:|:|:|")
 
-volume = tire_volume_function(width_tire, aspect_tire, diameter_tire)
-volume = round(volume, 1)
-print(f"The approximate volume is {volume} milliliters.")
+def main():
+    width = int(input("Enter the width of the tire in mm: "))
+    respective_ratio = int(input("Enter the aspect ratio of the tire: "))
+    respective_diameter = int(input("Enter the diameter of the wheel in inches: "))
 
-while loop:
-    buy = input("Would you like to buy tires with these dimensions? (yes/no) ")
-    if buy.lower() == "yes":
+    volume = round(((math.pi * width ** 2 * respective_ratio * (width * aspect_ratio + 2540 * respective_diameter)) / 10000000), 1)
+
+    print(f"The respective volume is {volume} milliliters.")
+    repeat()
+
+def repeat():
+    answer = input("Would you like to compute another volume (Y/N)? ")
+    if answer.upper() == "Y":
+        main()
+    if answer.upper() == "N":
+        print("The program will now end")
+        exit()
+    else:
+        print("Error: Invalid Argument")
+        repeat()
+
+while running:
+    shopping = input("Would you like to buy tires with these dimensions? (yes/no) ")
+    if shopping.lower() == "yes":
         print("Please enter a phone number, and we will contact you at a later time:")
         phone_number = input(">> ")
         print("Have a great day!")
-        loop = False
-    elif buy.lower() == "no":
+        loop = True
+    elif shopping.lower() == "no":
         print("Have a great day!")
         phone_number = ("")
         loop = False
@@ -34,3 +50,4 @@ current_date_and_time = datetime.now()
 
 with open("tire_volume.txt", "at") as dimens_file:
     print(f"\n{current_date_and_time}\n{width_tire}, {aspect_tire}, {diameter_tire}, {volume}\n{phone_number}", file=dimens_file)
+main()
